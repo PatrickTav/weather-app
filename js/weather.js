df4392cadfd8bed68acad13546f73099e697b2a1
Patrick Tavares <patricktavares3pc@gmail.com>
2022-04-12 20:30:40 -0300

    retirando espaços

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,7 @@
 const myKey = 'TIajKP5A7lCNWVNB7LfYHqx9H4FQgikb'
 
-
 const getCityUrl = cityName =>
     `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${myKey}&q=${cityName}`
-
-
-
 
 const fetchData = async (url) =>{
     try {
@@ -21,15 +17,12 @@ const fetchData = async (url) =>{
     }
 }
 
-
 const getCityData = cityName => fetchData(getCityUrl(cityName))
-
 
 const getCityWeather = async cityName =>{
     const [cityData] = await getCityData(cityName)
     const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${cityData.Key}?apikey=${myKey}&language=pt-br`
     return await fetchData(cityWeatherUrl)
-    
 }   
 
 
